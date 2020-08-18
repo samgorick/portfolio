bb9a9898d4039c8a0991be56cf081f8eb7a9fb32
gozmeister <samgorick.sg@gmail.com>
2020-08-18 17:17:38 -0500

    built header component

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+  return (
+      <div style={{textAlign: 'center'}}>
+    <h1>Welcome!</h1>
+    <p>Portfolio for Sam Gorick</p>
+    <p>Full Stack Developer // Product Manager</p>
+    <p>Chicago // London</p>
+  </div>
+  )
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 import Projects from "../components/projects"
+import Header from "../components/header"
 
 export default function Home() {
   return (
-    <Layout style={{ color: `purple` }}>
-      <h1>Welcome!</h1>
-      <p>Portfolio for Sam Gorick</p>
-      <p>Full Stack Developer // Product Manager</p>
-      <p>Chicago // London</p>
+    <Layout>
+      <Header />
       <Projects />
     </Layout>
   )
