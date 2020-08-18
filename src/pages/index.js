4a1708881c4c7c7bdcf970cc7629e081921bc821
gozmeister <samgorick.sg@gmail.com>
2020-08-18 12:17:42 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </Layout>
+  )
 }
